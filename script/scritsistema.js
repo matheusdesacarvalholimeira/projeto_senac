806f4042f1f0fa5f12b7e84daf3c578ec442b4b3
Matheus <118131034+matheusdesacarvalholimeira@users.noreply.github.com>
2023-04-28 10:23:40 -0300

    atualizacao

---
+++ script/scritsistema.js
@@ -0,0 +1,14 @@
+let bt = document.querySelector('#bto_add')
+      
+bt.addEventListener("click", ()=>{
+ 
+  if(document.querySelector('.desa').style.display == 'none'){
+    document.querySelector('.desa').style.display = 'block'
+  }else{
+    document.querySelector('.desa').style.display = 'none'
+  }
+
+
+
+})
+
